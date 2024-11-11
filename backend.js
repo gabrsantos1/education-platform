f168e941e36d38510e85e96380ec7f54b3b4b3b1
Pedro Paulo Barbosa Arantes <167035129+PedroPauloBarbosaArantes@users.noreply.github.com>
2024-11-11 00:07:04 -0300

    Update backend.js

--- backend.js
+++ backend.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '!Oupa1324',
+    password: '', // senha do seu banco de dados
     database: 'feedback_db'
 });
 
